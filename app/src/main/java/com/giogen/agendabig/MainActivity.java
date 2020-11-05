9184b9a52e53724df1a9bd6606dc82a024f5ea3b
Giovanni Medina <grablens_war@hotmail.com>
2020-11-05 14:20:10 -0600

    Realizando commit

--- app/src/main/java/com/giogen/agendabig/MainActivity.java
+++ app/src/main/java/com/giogen/agendabig/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hola a todos como andamos
+        //Hola a todos como andamos aqui
         int Calis;
     }
 }
