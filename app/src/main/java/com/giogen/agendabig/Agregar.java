823b1c8d0dc9885930a4921a6ff10b298d1edbf0
Giovanni Medina <grablens_war@hotmail.com>
2020-11-06 00:00:49 -0600

    Agregando xml de de actividad de agregar, se crearon los metodos de agregar y eliminar de una Ficha

--- app/src/main/java/com/giogen/agendabig/Agregar.java
+++ app/src/main/java/com/giogen/agendabig/Agregar.java
@@ -1,4 +1,14 @@
 package com.giogen.agendabig;
 
-public class Agregar {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Agregar extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar);
+    }
 }
--- app/src/main/java/com/giogen/agendabig/ObjetosYDaos/DaoFicha.java
+++ app/src/main/java/com/giogen/agendabig/ObjetosYDaos/DaoFicha.java
@@ -1,4 +1,42 @@
 package com.giogen.agendabig.ObjetosYDaos;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class DaoFicha {
+
+    private SQLiteDatabase bd;
+
+    public DaoFicha(Context context){
+        this.bd=new BaseDeDatos(context).getWritableDatabase();
+    }
+
+    //Metodo para insertar una ficha en nuestra base de datos.
+    public long Insert(Ficha ficha){
+        ContentValues cnt=new ContentValues();
+        cnt.put(BaseDeDatos.FICHACOLUMNS[0],ficha.getTitulo());
+        cnt.put(BaseDeDatos.FICHACOLUMNS[1],ficha.getDescripcion());
+        cnt.put(BaseDeDatos.FICHACOLUMNS[2],ficha.getTipo());
+        cnt.put(BaseDeDatos.FICHACOLUMNS[3],ficha.getFechacreacion());
+        cnt.put(BaseDeDatos.FICHACOLUMNS[4],ficha.getFechaRecordatorio());
+        cnt.put(BaseDeDatos.FICHACOLUMNS[5],ficha.getEstado());
+
+        return  bd.insert(BaseDeDatos.FICHA,null,cnt);
+    }
+
+    //Metodo para eliminar una ficha, se debe eliminar todos los archivos que este contenga, y aque tiene una llave foranea
+    public boolean eliminar(Ficha ficha){
+        int no=bd.delete(BaseDeDatos.ARCHIVO,BaseDeDatos.ARCHIVOCOLUMNS[4]+"=?",new String[]{ficha.getTitulo()});
+        if(no>0){
+            int no1=bd.delete(BaseDeDatos.FICHA,BaseDeDatos.FICHACOLUMNS[0]+"=?",new String[]{ficha.getTitulo()});
+            if(no1>0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
 }
