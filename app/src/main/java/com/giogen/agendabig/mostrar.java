d38eb78c2127ad07d3ad37f68ccd67336af6d17b
EdgarBryanLM <55665769+EdgarBryanLM@users.noreply.github.com>
2020-11-06 10:36:21 -0600

    Comentario

--- app/src/main/java/com/giogen/agendabig/mostrar.java
+++ app/src/main/java/com/giogen/agendabig/mostrar.java
@@ -1,4 +1,18 @@
 package com.giogen.agendabig;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class mostrar {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.giogen.agendabig.ObjetosYDaos.Archivo;
+import com.giogen.agendabig.ObjetosYDaos.DaoArchivo;
+import com.giogen.agendabig.ObjetosYDaos.DaoFicha;
+import com.giogen.agendabig.ObjetosYDaos.Ficha;
+
+import java.util.ArrayList;
+public class mostrar extends AppCompatActivity {
+
+    
 }
